25cb0caf34784c89b66813bf0a9e54736b61b8dc
Tatiana <tatum@rambler.ru>
2022-11-28 18:03:09 +0300

    Task 2

---
+++ Sem1Task2dz.java
@@ -0,0 +1,19 @@
+//Вывести все простые числа от 1 до 1000
+
+
+public class Sem1Task2dz {
+    public static void main(String[] args) {
+        for (int i = 2; i < 1001; i ++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j ++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + " ");;
+            }
+        }
+    }
+}
